67277c59641cc5928d64170ceb934ba91f269926
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-01 21:46:25 +0530

    Add Check Array Sorted

---
+++ Arrays/ArrayProblems/CheckArraySorted.cpp
@@ -0,0 +1,8 @@
+bool isSorted(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+            return false;
+    }
+
+    return true;  // Return true if the array is sorted
+}
