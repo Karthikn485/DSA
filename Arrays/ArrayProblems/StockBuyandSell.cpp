b2bcec69404ec90bb21ef1e446ed9903e448b48e
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-02 22:24:34 +0530

    Add Stock Buy and Sell

---
+++ Arrays/ArrayProblems/StockBuyandSell.cpp
@@ -0,0 +1,12 @@
+int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int maxprofit=0;
+        int mini=prices[0];
+        for(int i=1;i<n;i++ ){
+            int cost=prices[i]-mini;
+            maxprofit=max(maxprofit,cost);
+            mini=min(mini,prices[i]);
+        }
+        return maxprofit;
+        
+    }
