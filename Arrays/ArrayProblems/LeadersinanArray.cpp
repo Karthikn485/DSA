ef36d1866dbcdc2e4685e5d246e53d8dd5de5e14
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-03 22:07:32 +0530

    Add Leaders

---
+++ Arrays/ArrayProblems/LeadersinanArray.cpp
@@ -0,0 +1,15 @@
+ vector<int> leaders(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans;
+        int maxi=INT_MIN;
+        for(int i=n-1;i>=0;i--){
+            if(nums[i]>maxi){
+                ans.push_back(nums[i]);
+            }
+            maxi=max(maxi,nums[i]);
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+
+      
+    }
