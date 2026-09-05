2a8d44380fbe71a8109d8dcd58fb569f4a514d78
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-05 13:48:35 +0530

    Add upper bound

---
+++ BinarySearch/UpperBound.cpp
@@ -0,0 +1,18 @@
+ int upperBound(vector<int> &nums, int x){
+            int low=0;
+        int n=nums.size();
+        int ans=n;
+        int high=n-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]>x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+
+        }
+        return ans;
+    }
