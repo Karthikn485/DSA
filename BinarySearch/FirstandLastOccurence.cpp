70f688ee4b7bce62c3671f850fa69a24d1f68e4c
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-07 13:07:49 +0530

    Add First and Last Occurence

---
+++ BinarySearch/FirstandLastOccurence.cpp
@@ -0,0 +1,52 @@
+class Solution{
+public:
+    int firstOcc(vector <int> &nums, int x){
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        int first=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==x){
+                first=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return first;
+    }
+     int lastOcc(vector <int> &nums, int x){
+        int low=0;
+        int n=nums.size();
+        int high=n-1;
+        int last=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==x){
+                last=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<x){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return last;
+    }
+    vector<int> searchRange(vector<int> &nums, int target) {
+        int first=firstOcc(nums,target);
+        if(first==-1){
+          return {-1,-1};
+ 
+        }
+        int last=lastOcc(nums,target);
+        return{first,last};
+    }
+};
