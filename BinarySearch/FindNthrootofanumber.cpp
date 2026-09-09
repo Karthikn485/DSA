4cea0acd2bfabd3e5990b46e01bfbe64a270e28f
KARTHIK.N <153536402+Karthikn485@users.noreply.github.com>
2026-09-09 19:16:38 +0530

    ADD nth root of a number

---
+++ BinarySearch/FindNthrootofanumber.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+   int func(int mid,int n,int m){
+    int ans=1;
+    for(int i=1;i<=n;i++){
+        ans=ans*mid;
+        if(ans>m){
+            return 2;
+        }
+    }
+    if(ans==m) return 1;
+    return 0;
+   }
+  int NthRoot(int N, int M) {
+    int low=1;
+    int high=M;
+    while(low<=high){
+        int mid=(low+high)/2;
+        int midN=func(mid,N,M);
+        if(midN==1){
+            return mid;
+        }
+        else if(midN==0){
+            low=mid+1;
+        }
+        else{
+            high=mid-1;
+        }
+    }
+    return -1;
+       
+    }
+};
